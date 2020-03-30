8b32e5c54781b132047588c9e440c16fa0fd3cd9
latinawhocodes <alyssaroque01@gmail.com>
2020-03-29 20:54:13 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+let dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+    let leftPosition = dodger.style.left.replace("px", "");
+    let left = parseInt(leftPosition, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    let rightPosition = dodger.style.left.replace("px", "");
+    let right = parseInt(rightPosition, 10);
+
+    if (right < 360) {
+        dodger.style.left = `${right + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+
+    if (event.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
